c003bd74b98bc790a9a66713c533ad00f09a8c6b
Your Name <you@example.com>
2020-05-06 23:10:34 +0430

    add more section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss'],
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
-  constructor() {}
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
+    constructor() {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
+    CreateIngredient(data: {
+        ingredientName: string;
+        ingredientAmount: number;
+    }) {
+        this.ingredients.push(
+            new Ingredient(data.ingredientName, data.ingredientAmount)
+        );
+    }
+
 }
